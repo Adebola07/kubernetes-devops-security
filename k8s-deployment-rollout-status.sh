1fc1252e5774c8cdb652e94ed4f702d10f6de8d9
Adebola07 <07zamani@gmail.com>
2024-06-23 05:03:35 -0400

    repo refactoring

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -6,7 +6,7 @@ sleep 60s
 
 if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
-	echo "Deployment ${deploymentName} Rollout has Failed"
+    echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n default rollout undo deploy ${deploymentName}
     exit 1;
 else
